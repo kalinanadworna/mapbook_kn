d6ed2780ebd39de059144b11bad66f054d060ea7
kalinanadworna <kalina.nadworna1@o2.pl>
2025-04-16 11:13:12 +0200

    wip: new function: remove user

--- utils/controller.py
+++ utils/controller.py
@@ -1,1 +1,5 @@
-
+def remove_user(users_data: list) -> None:
+    user_name = input("podaj imie użytkownika do usunięcia: ")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
